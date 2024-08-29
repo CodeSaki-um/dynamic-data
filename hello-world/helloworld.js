09e16d4e33da4a9b086295ae60dc971be6534531
CodeSaki-um <Samcagui8@gmail.com>
2024-08-29 19:51:47 -0400

    Create helloworld.js

---
+++ hello-world/helloworld.js
@@ -0,0 +1,21 @@
+const http = require('http')
+//^crucial for any server
+
+const PORT = process.env.PORT || 3000
+
+const server = http.createServer((request,response) => {
+    console.log(request)
+    console.log("responding to request")
+    response.writeHead(200,{'Content-Type':'text/plain'})
+    response.end('Hello World')
+})
+
+server.listen(PORT, ()=> console.log('server started on port ${PORT}; ' + 'press Ctrl-c to terminate.....'))
+
+// var name = "john" 
+// var age = 23.5
+// let lastName = "smith"
+// const year = 2024
+
+// javascript object notation aka json
+// var printer = { color:"black", size:"small", price:29.99}
