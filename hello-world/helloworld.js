005021238d46039b497a54eebd1aee1937067b81
CodeSaki-um <Samcagui8@gmail.com>
2024-09-05 17:37:40 -0400

    Update helloworld.js

--- hello-world/helloworld.js
+++ hello-world/helloworld.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000
 const server = http.createServer((request,response) => {
     console.log(request)
     console.log("responding to request")
-    response.writeHead(200,{"Content-Type':'text/plain"})
+    response.writeHead(200,{"Content-Type":"text/plain"})
     response.end("Hello World")
 })
 
