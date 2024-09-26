b2dab7e71f40a3d3c5f240f68a605c68a3ad24e7
CodeSaki-um <Samcagui8@gmail.com>
2024-09-26 18:14:50 -0400

    lab 5

--- miami/site/miami.js
+++ miami/site/miami.js
@@ -1,47 +1,44 @@
-//imports express into our project
-const express = require('express')
-
+// imports express into our project 
+const express = require('express') 
+//create the express server inside a variable called app
 const app = express()
-//import a package for handlebars
+// import a package for handlebars
 const expressHandlebars = require('express-handlebars')
-//make express use the handlebars template engine
-app.engine('handlebars', expressHandlebars.engine({
-   defaultLayout: 'main',
+// make express use the handlebars template engine
+app.engine('handlebars',expressHandlebars.engine({
+    defaultLayout: 'main',
 }))
-app.set('view engine', 'handlebars')
-
+app.set('view engine','handlebars')
 
 const PORT = process.env.port || 3000
 //process routes before error
 app.get('/',(request,response)=>{
-  response.render('home')
+    response.render('home')
 })
-
 app.get('/about',(request,response)=>{
-   response.render('about')
+    response.render('about')
 })
-
-app.get('/gallery',(request,response)=>{
-   response.render('gallery')
-})
-
 app.get('/nightlife',(request,response)=>{
-   response.render('nightlife')
+    response.type('text/plain')
+    response.send('Miami At Night')
 })
-
-//this triggers a server error req=/ request res=/ response
+app.get('/beaches',(request,response)=>{
+    response.type('text/plain')
+    response.send('Miami Beach and more!')
+})
+//this triggers a server error
 app.get('/history',(req,res)=>{
-   response.type('text/plain')
-   response.send('Miami Clubs to Visit!')
+    response.type('text/plain')
+    response.send('History of Miami')
 })
-
-//handle errors first
-app.use( (request, response)=>{
+//Handle the error first
+//NOT FOUND!
+app.use((request,response)=>{
     response.status(404)
     response.render('404')
  })
 
- //server error :(
+ //SERVER ERROR :(
  app.use((error,request,response,next)=>{
     console.log(error.message)
     response.status(500)
@@ -49,6 +46,7 @@ app.use( (request, response)=>{
  })
 
  app.listen(PORT, ()=>{
-    console.log(`Express is running on http://localhost:${PORT}`)
+    console.log(`Express is running on http://localhost:${PORT} `)
     console.log('Press ctrl-c to terminate')
  })
+
