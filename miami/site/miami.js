25c736e91248ae671052e59f77e60fac97469e61
CodeSaki-um <Samcagui8@gmail.com>
2024-10-17 19:47:15 -0400

    lab 7 stuff

--- miami/site/miami.js
+++ miami/site/miami.js
@@ -26,43 +26,55 @@ const gallery = require("./data/gallery.json")
 //process routes before error
 app.get('/',(request,response)=>{
     console.log(gallery)
+    const data = require("./data/home-data.json")
     response.render('landing',{
         gallery,
-        title:"This is Miami!",
-        abstract:"Miami is a great place to live.",
-        image:"mbsky.jpg"
+        data
         })
 })
 
 app.get('/about',(request,response)=>{
-    response.render('page',{
+    response.render('landing',{
         title:"About Miami",
         abstract:"From the Miami Marlins to the Miami Heat, there is a lot to see."
     })
 })
 
-app.get('/nl',(request,response)=>{
-    response.render('page',{
-        title:"About Miami's Nightlife",
-        abstract:"Club Space, Level2, and Many More Clubs Ready for You to Explore."
+app.get('/beach',(request,response)=>{
+    const data = require("./data/beach-data.json")
+    response.render('landing',{
+        data
     })
 })
 
-app.get('/page',(request,response)=>{
-    response.render('page',{
+app.get('/culin',(request,response)=>{
+    response.render('landing',{
         title:"Miami is a sleepless city, vibes that last a lifetime!",
         abstract:"Day stuff"
     })
 })
 
-app.get('/galp',(request,response)=>{
+app.get('/cult',(request,response)=>{
     console.log(gallery)
-    response.render('page',{
+    response.render('landing',{
         gallery,
         title:"This is a Gallery",
         abstract:"A bunch of pics.",
         image:"mbsky.jpg"
         })
+})
+
+app.get('/nl',(request,response)=>{
+    response.render('landing',{
+        title:"About Miami's Nightlife",
+        abstract:"Club Space, Level2, and Many More Clubs Ready for You to Explore."
+    })
+})
+
+app.get('/gal',(request,response)=>{
+    response.render('landing',{
+        title:"Gallery"
+    })
 })
 
 //this triggers a server error
